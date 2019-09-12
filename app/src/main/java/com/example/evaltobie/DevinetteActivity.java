74b254ad3fc9844577edefa6b9f6b0b8769427e1
tobie.tourre <tobie.tourre@ipilyon.net>
2019-09-12 16:22:39 +0200

    devinette

---
+++ app/src/main/java/com/example/evaltobie/DevinetteActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaltobie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DevinetteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_devinette);
+    }
+}
