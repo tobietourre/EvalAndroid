a07c0a73d38268c882abef8688a15894e75a488a
tobie.tourre <tobie.tourre@ipilyon.net>
2019-09-12 14:34:48 +0200

    calculette + menu

--- app/src/main/java/com/example/evaltobie/MainActivity.java
+++ app/src/main/java/com/example/evaltobie/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.evaltobie;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goCalculette(View v){
+        Intent intent = new Intent(this, CalculetteActivity.class);
+        startActivity(intent);
+    }
+
+    public void goTva(View v){
+        Intent intent = new Intent(this, TvaActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/evaltobie/TvaActivity.java
@@ -0,0 +1,14 @@
+package com.example.evaltobie;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TvaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tva);
+    }
+}
